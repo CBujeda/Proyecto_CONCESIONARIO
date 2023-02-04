a9d669e1a4970b1b8b554dd28c79663c041698c6
elisuco6 <elisalexandru32@gmail.com>
2023-02-04 13:02:33 +0100

    Tablas Estiladas
    Hacemos las tablas mas guapas del universo de las naves espaciales del inframundo.

--- CONCESIONARIO_PROYECTO/CONCESIONARIO_PROYECTO/Concesionario.aspx
+++ CONCESIONARIO_PROYECTO/CONCESIONARIO_PROYECTO/Concesionario.aspx
@@ -4,13 +4,28 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script src="Scripts/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    </body>
+    <title>ConcesionariusMaximus</title>
+
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            <asp:GridView ID="concesionarioTabla" runat="server"></asp:GridView>
+            <asp:GridView class="table table-dark table-hover table-striped" ID="concesionarioTabla" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="idVehiculo" HeaderText="ID" />
+                    <asp:BoundField DataField="nombreVehiculo" HeaderText="Nombre Vehiculo" />
+                    <asp:BoundField DataField="tipoVehiculo" HeaderText="Tipo de Vehiculo" />
+                    <asp:BoundField DataField="modelo" HeaderText="Modelo" />
+                    <asp:BoundField DataField="motor" HeaderText="Motor" />
+                    <asp:BoundField DataField="nombreMarca" HeaderText="Marca" />
+                    <asp:BoundField DataField="pais" HeaderText="Pais" />
+                    <asp:BoundField DataField="anno" HeaderText="Año" />
+                </Columns>
+            </asp:GridView>
         </div>
     </form>
 </body>
--- CONCESIONARIO_PROYECTO/CONCESIONARIO_PROYECTO/Concesionario.aspx.cs
+++ CONCESIONARIO_PROYECTO/CONCESIONARIO_PROYECTO/Concesionario.aspx.cs
@@ -10,15 +10,33 @@ namespace CONCESIONARIO_PROYECTO
 {
     public partial class Concesionario : System.Web.UI.Page
     {
+        public void CreateDatabase()
+        {
+            ConcesionarioRepositoryDataContext db = new ConcesionarioRepositoryDataContext();
+            if (!db.DatabaseExists())
+            {
+                //db.DeleteDatabase();
+                db.CreateDatabase();
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            CreateDatabase();
             ConcesionarioRepositoryDataContext dbRep = new ConcesionarioRepositoryDataContext();
             concesionarioTabla.DataSource = from Vehiculos in dbRep.Vehiculos
                                             join Modelos in dbRep.Modelos on Vehiculos.id_modelo equals Modelos.id_modelo
                                             join Marcas in dbRep.Marcas on Modelos.id_marca equals Marcas.id_marca
-                                            select new { Vehiculos.nombre, Modelos.motor, nombreMarca = Marcas.nombre };
-
+                                            select new {idVehiculo = Vehiculos.id_vehiculo,
+                                                        nombreVehiculo = Vehiculos.nombre,
+                                                        tipoVehiculo = Vehiculos.tipo,
+                                                        Modelos.modelo,
+                                                        Modelos.motor,
+                                                        nombreMarca = Marcas.nombre,
+                                                        Marcas.pais, 
+                                                        anno = Marcas.anno_creacion};
             concesionarioTabla.DataBind();
         }
+
     }
 }
